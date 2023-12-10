79cbd23e6236a5b8b367876f37190c983cfd3449
jochensambaer1 <jochen_sambaer@hotmail.com>
2023-12-10 20:28:45 +0100

    Update package name and fix layout in lesson 02 and 03

--- src/components/lessons/03/lesson-03.tsx
+++ src/components/lessons/03/lesson-03.tsx
@@ -4,6 +4,7 @@ import { Task03 as Task } from '../../tasks/03/task-03';
 import { Pupil } from '../pupil/pupil';
 import styles from './lesson-03.module.scss';
 
+
 export const Lesson03 = () => {
     const [lessonSolved, setLessonSolved] = useState(false);
 
@@ -15,10 +16,11 @@ export const Lesson03 = () => {
         <div className={styles.root}>
             <Task />
             <div className={styles.playground}>
-                <div id="top" className={`${styles.eye} ${styles.top}`} />
-                <div id="bottom" className={`${styles.eye} ${styles.bottom}`}>
-                    <Pupil color={'hotRed'} />
+                <div id="top" className={`${styles.eye} ${styles.top}`}>
+                    <Pupil />
                 </div>
+                <div id="bottom" className={`${styles.eye} ${styles.bottom}`}>
+                    <Pupil color={'hotRed'} /></div>
                 <ConfettiFx
                     maxParticles={400}
                     dissolve={315}
